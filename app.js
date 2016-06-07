53332f52294f7e127a4eb03ff40e49e778c4859e
Craig Franklin <craigjfranklin@gmail.com>
2016-06-07 11:36:13 +1000

    clean up structure

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express, path, favicon, logger, cookieParser, bodyParser, routes,
+  app;
 
-var app = express();
+express = require('express');
+path = require('path');
+favicon = require('serve-favicon');
+logger = require('morgan');
+cookieParser = require('cookie-parser');
+bodyParser = require('body-parser');
+
+routes = require('./routes/index');
+app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+var express, router;
+
+express = require('express');
+router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
